fccf0d1c802cf10852c647a39c1cc26003d00e18
岩塚由楽 <iwatsukayura@gmail.com>
2025-06-13 14:21:22 +0900

    test

--- chapter04.go
+++ chapter04.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	for i:=0; i<3;i++{
-		wg.Add(1)
-		go func(i int){
-			defer wg.Done()
-			fmt.Println(i)
-		}(i)
+	src := []int{1, 2, 3, 4, 5}
+	dst := []int{}
+
+	c := make(chan int)
+
+	for _, s := range src {
+		go func(s int, c chan int) {
+			result := s * 2
+			c <- result
+		}(s, c)
 	}
 
-	wg.Wait()
+	for _ = range src {
+		num := <-c
+		dst = append(dst, num)
+	}
+
+	fmt.Println(dst)
+	close(c)
 }
